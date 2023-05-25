21f62e56d39faf8088815e0f5a6bc61b06ce411e
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2023-05-24 18:09:14 -0700

    dep

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// const imgs = document.querySelector("work-img");
+// const fullPage = document.querySelector("full-page");
+
+// imgs.forEach((img) => {
+//   img.addEventListener("click", function () {
+//     fullPage.getElementsByClassName.backgroundImage = "url(" + img.src + ")";
+//     fullPage.style.display = "block";
+//   });
+// });
